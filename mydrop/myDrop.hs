c849ddbb8f6e904815c1cc200b17bb3950070517
Gabriel Lett <glett@bionexo.com>
2014-12-23 10:57:20 -0200

    Creating myDrop function, with recursion that contains a recursion that contains a recursion that contais a recursion tha...

---
+++ mydrop/myDrop.hs
@@ -0,0 +1,4 @@
+-- Function myDrop recieves n and 'x'es
+myDrop n xs = 	if n <= 0 || null xs -- If number to drop is <=0 or null
+		then xs -- returns xs
+		else myDrop (n - 1) (tail xs) -- else call recursivelly myDrop passing n-1 and the tail of 'x'es list
