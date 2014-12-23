a4b44c954825e0967ddea1334e1dbc84416e4099
Gabriel Lett <glett@bionexo.com>
2014-12-23 15:41:16 -0200

    Adding one line version of the function

--- mydrop/myDrop.hs
+++ mydrop/myDrop.hs
@@ -2,3 +2,9 @@
 myDrop n xs = 	if n <= 0 || null xs -- If number to drop is <=0 or null
 		then xs -- returns xs
 		else myDrop (n - 1) (tail xs) -- else call recursivelly myDrop passing n-1 and the tail of 'x'es list
+
+-- This function could be defined like this:
+--
+-- myDropX n xs = if n <= 0 || null xs then xs else myDropX (n - 1) (tail xs)
+--
+-- but it lacks readabillity
